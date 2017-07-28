fe4bc4dd509271e4bc179df0e01279f51a8fc0b4
Marcel Menzel <mail@mcl.gg>
2017-07-29 00:39:57 +0200

    magik with gm now

--- commands/magik.js
+++ commands/magik.js
@@ -3,9 +3,10 @@ const path = require('path');
 const child = require('child_process');
 const fs = require('fs');
 const axios = require('axios');
+const gm = require('gm').subClass({ imageMagick: true });
 
 const opts = {
-  stdio: ['pipe', 'pipe', 'pipe'],
+  stdio: [0, 1, 2, 'pipe', 'pipe'],
   end: false,
 };
 
@@ -30,57 +31,83 @@ commands.magik = {
   hide: true,
   args: ['imagemagick'],
   desc: 'applies some magik to an image',
-  fn: (message, params, main) => {
-    message.send('ok, processing')
-      .then((msg) => {
-        const comb1 = random(combinations);
-        const comb2 = random(combinations);
+  fn: async (message, params, main) => {
+    const waitmsg = await message.send('ok, processing');
 
-        const selectedDimensions = `${comb1[0]}%x${comb2[0]}%`;
-        const selectedDimensions2 = `${comb1[1]}%x${comb2[1]}%`;
+    const comb1 = random(combinations);
+    const comb2 = random(combinations);
 
-        const magik = child.spawn('convert', ['(', '(', 'fd:0', '-resize', '2000x2000>', ')', '-liquid-rescale', selectedDimensions, ')', '-resize', selectedDimensions2, 'fd:1'], opts);
+    const selectedDimensions = `${comb1[0]}%x${comb2[0]}%`;
+    const selectedDimensions2 = `${comb1[1]}%x${comb2[1]}%`;
 
-        // const magik = child.spawn('convert', ['fd:0', '-resize', '64x64', 'fd:1'], opts);
 
-        axios({
-          method:'get',
-          url:params,
-          responseType:'stream'
-        })
-          .then(function(response) {
-            response.data.pipe(magik.stdio[0])
-          });
+    // const magik = child.spawn('convert', ['(', '(', 'fd:3', '-resize', '2000x2000>', ')', '-liquid-rescale', selectedDimensions, ')', '-resize', selectedDimensions2, 'fd:4'], opts);
+    // const magik = child.spawn('convert', ['fd:3', '-resize', '2000x2000>', '-liquid-rescale', selectedDimensions, '-resize', selectedDimensions2, 'fd:4'], opts);
 
-        // const buf = require('fs').readFileSync('./imgtmp/lel.jpg');
 
-        const childStream = [];
+    // const magik = child.spawn('convert', ['fd:0', '-resize', '64x64', 'fd:1'], opts);
 
-        // magik.stdio[0].write(buf);
-        // magik.stdio[0].end();
+    const httpResponse = await axios({
+      method: 'get',
+      url: params,
+      responseType: 'stream',
+    });
 
-        magik.stdio[1].on('data', (chunk) => {
-          childStream.push(chunk);
+    // const inputStream = new net.Socket({ fd: 3, readable: true, writable: true });
+
+    // httpResponse.data.pipe(magik.stdio[3]);
+
+    gm(httpResponse.data)
+      .resize(2000, 2000, '>')
+      .in(`-liquid-rescale`, selectedDimensions)
+      .resize(comb1[1], comb2[1], '%')
+      .toBuffer(async (err, buffer) => {
+        if (err) return console.log('Error in magick;', err);
+        await message.send({
+          files: [{
+            attachment: buffer,
+            name: 'magik.png',
+          }],
         });
 
-        magik.stdio[1].on('end', () => {
-          console.log('fin rcvd data:', childStream);
-
-          const buf = Buffer.concat(childStream);
-
-          console.log(buf.length);
-
-          // fs.createWriteStream('tmp.png').write(buf);
-
-          message.send({
-            files: [{
-              attachment: buf,
-              name: 'magik.png',
-            }],
-          })
-            .then(() => msg.delete());
-        });
+        waitmsg.delete();
+        delete message.replies[0];
       });
+
+    // const buf = require('fs').readFileSync('./imgtmp/lel.jpg');
+
+    // const childStream = [];
+
+    // magik.stdio[0].write(buf);
+    // magik.stdio[0].end();
+
+    // magik.stdio[3].on('error', (err) => {
+    //   console.log('fd3 error:', err);
+    // });
+    //
+    // magik.stdio[4].on('data', (chunk) => {
+    //   childStream.push(chunk);
+    // });
+    //
+    // magik.stdio[4].on('end', async () => {
+    //   console.log('fin rcvd data:', childStream);
+    //
+    //   const buf = Buffer.concat(childStream);
+    //
+    //   console.log(buf.length);
+    //
+    //   // fs.createWriteStream('tmp.png').write(buf);
+    //
+    //   const magikMessage = await message.send({
+    //     files: [{
+    //       attachment: buf,
+    //       name: 'magik.png',
+    //     }],
+    //   });
+    //
+    //   waitmsg.delete();
+    //   delete message.replies[0];
+    // });
   },
 };
 
