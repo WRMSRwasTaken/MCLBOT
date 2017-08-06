38280c1adc9534f8f149ed35547c66798ec1b9d9
Marcel Menzel <mail@mcl.gg>
2017-08-06 15:34:26 +0200

    more stats

--- tasks/updateMetrics.js
+++ tasks/updateMetrics.js
@@ -4,10 +4,10 @@ tasks.updateMetrics = {
   name: 'updateMetrics',
   interval: 60,
   fn: (main) => {
-    main.prometheusMetrics.serverCountGauge.set(main.api.guilds.size);
-    main.prometheusMetrics.userCacheCountGauge.set(main.api.users.size);
+    main.prometheusMetrics.serverCountGauge.set(main.api.guilds.size || 0);
+    main.prometheusMetrics.userCacheCountGauge.set(main.api.users.size || 0);
     main.prometheusMetrics.uptime.set(Date.now() - main.startTime);
-    main.prometheusMetrics.latency.set(main.api.ping);
+    main.prometheusMetrics.latency.set(main.api.ping || 0);
   },
 };
 
