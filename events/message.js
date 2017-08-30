2c070c781e9364ceb4ec7313bfc76c7bc7f4621a
Marcel Menzel <mail@mcl.gg>
2017-08-30 22:13:37 +0200

    better

--- events/message.js
+++ events/message.js
@@ -3,13 +3,6 @@ module.exports = {
     if (message.channel.type !== 'dm' && !message.author.bot) {
       main.prometheusMetrics.sqlWrites.inc();
 
-      main.db.member_last_message.upsert({
-        server_id: message.guild.id,
-        user_id: message.author.id,
-      });
-
-      main.prometheusMetrics.sqlWrites.inc();
-
       main.db.member_message.upsert({
         server_id: message.guild.id,
         user_id: message.author.id,
