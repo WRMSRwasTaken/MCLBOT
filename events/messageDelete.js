883372f2c2f6a29c5557ca0210719db3c22f95c0
Marcel Menzel <mail@mcl.gg>
2017-12-02 21:36:41 +0100

    fix prometheus export for prometheus 2.0 & some influxdb improvements

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,5 +1,11 @@
 module.exports = {
   fn: (main, message) => {
+    if (message.guild && !message.author.bot) {
+      main.prometheusMetrics.influxWrites.inc();
+
+      main.influx.query(`delete from member_message where message_id = ${main.Influx.escape.stringLit(message.id)}`);
+    }
+
     main.commandHandler.handleMessageDeleteEvent(message);
   },
 };
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,5 +1,31 @@
 module.exports = {
   fn: (main, oldMessage, newMessage) => {
+    if (newMessage.guild && !newMessage.author.bot) {
+      main.prometheusMetrics.influxWrites.inc();
+
+      main.influx.query(`delete from member_message where message_id = ${main.Influx.escape.stringLit(newMessage.id)}`);
+
+      main.prometheusMetrics.influxWrites.inc();
+
+      main.influx.writePoints([
+        {
+          measurement: 'member_message',
+          tags: {
+            guild_id: newMessage.guild.id,
+            user_id: newMessage.author.id,
+            channel_id: newMessage.channel.id,
+          },
+          fields: {
+            message_id: newMessage.id,
+            char_count: newMessage.content.length,
+            word_count: (newMessage.content.length) ? newMessage.content.split(' ').length : 0,
+            user_mention_count: newMessage.mentions.members.size + newMessage.mentions.roles.size + ((newMessage.mentions.everyone) ? newMessage.guild.memberCount : 0),
+            attachment_count: newMessage.attachments.size,
+          },
+        },
+      ]);
+    }
+
     main.commandHandler.handleMessageEvent(oldMessage, newMessage);
   },
 };
