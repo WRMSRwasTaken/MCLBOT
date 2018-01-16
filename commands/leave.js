ff6ec8b42d0a25f748e21baae15d21ca52a1c88c
Marcel Menzel <mail@mcl.gg>
2018-01-16 01:38:17 +0100

    this should be confirmed aswell

--- commands/leave.js
+++ commands/leave.js
@@ -2,5 +2,23 @@ module.exports = {
   desc: 'Leave the current server',
   guildOnly: true,
   permission: 'ADMINISTRATOR',
-  fn: async ctx => ctx.guild.leave(),
+  fn: async (ctx) => {
+    const msg = await ctx.reply('Do you really me to leave your server?');
+
+    const confirm = ctx.main.confirmationHelper.initConfirm(msg, ctx.author);
+
+    confirm.on('timeout', () => {
+      msg.edit('Server leave cancelled, due to input timeout.');
+    });
+
+    confirm.on('false', () => {
+      msg.edit('Server leave cancelled.');
+    });
+
+    confirm.on('true', async () => {
+      msg.edit('Leaving the server, bye bye!');
+
+      ctx.guild.leave();
+    });
+  },
 };
