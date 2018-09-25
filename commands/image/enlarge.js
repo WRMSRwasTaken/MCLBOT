dc1f7d5c6e5809a476384e8be282e5437a778004
Marcel Menzel <mail@mcl.gg>
2018-09-25 21:46:53 +0200

    Some image handling tweaks

--- commands/image/enlarge.js
+++ commands/image/enlarge.js
@@ -7,21 +7,5 @@ module.exports = {
       type: 'emoji',
     },
   ],
-  fn: async (ctx, emoji) => {
-    let emojiData;
-
-    try {
-      emojiData = await ctx.main.imageHelper.downloadImage(emoji);
-    } catch (ex) {
-      return ctx.reply(ex.message);
-    }
-
-    return ctx.reply({
-      files: [{
-        attachment: emojiData,
-        // name: 'emoji.png',
-      }],
-    });
-  },
+  fn: async (ctx, emoji) => ctx.main.imageHelper.downloadAndAttachImage(ctx, emoji),
 };
-
