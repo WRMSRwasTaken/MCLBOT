a04d8fc0ae43485bdb137a0e2b6889303b6838cc
Marcel Menzel <mail@mcl.gg>
2018-12-13 03:37:44 +0100

    fix "user last seen" stats collection

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -10,19 +10,19 @@ so we need some sort of a debounce system to wait for all presence updates to fi
 const pendingUpdates = {};
 
 module.exports = {
-  fn: (main, oldPresence, newPresence) => {
-    if (newPresence.status === 'offline') {
-      if (!pendingUpdates[newPresence.user.id]) {
+  fn: (main, oldMember, newMember) => {
+    if (newMember.presence.status === 'offline') {
+      if (!pendingUpdates[newMember.user.id]) {
         setTimeout(() => {
           main.prometheusMetrics.redisWrites.inc();
 
-          main.redis.set(`user_last_seen:${newPresence.user.id}`, pendingUpdates[newPresence.user.id], 'EX', nconf.get('bot:redisStoreTTL'));
+          main.redis.set(`user_last_seen:${newMember.user.id}`, pendingUpdates[newMember.user.id], 'EX', nconf.get('bot:redisStoreTTL'));
 
-          delete pendingUpdates[newPresence.user.id];
+          delete pendingUpdates[newMember.user.id];
         }, 5000);
       }
 
-      pendingUpdates[newPresence.user.id] = Date.now();
+      pendingUpdates[newMember.user.id] = Date.now();
     }
   },
 };
