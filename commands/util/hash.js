354082afe140d569392d0e490215a8ab725393d7
Marcel Menzel <mail@mcl.gg>
2018-12-18 04:19:18 +0100

    added hash command

---
+++ commands/util/hash.js
@@ -0,0 +1,36 @@
+const crypto = require('crypto');
+
+const hashes = crypto.getHashes();
+
+module.exports = {
+  description: 'Returns a hash for the specified input & algorithm',
+  arguments: [
+    {
+      label: 'algorithm',
+      type: 'string',
+    },
+    {
+      label: 'input',
+      type: 'string',
+      infinite: true,
+    },
+  ],
+  subcommands: {
+    list: {
+      description: 'List all supported hashing algorithms',
+      alias: 'l',
+      fn: async () => `Supported hashing algorithms (case sensitive):\n\n${hashes.join(', ')}`,
+    },
+  },
+  fn: async (ctx, algorithm, input) => {
+    if (!hashes[algorithm]) {
+      return ctx.main.stringUtils.argumentError(ctx, 0, 'Invalid hash algorithm given. Use `hash list` to get a list of supported hashing algorithms');
+    }
+
+    const hash = crypto.createHash(algorithm);
+
+    hash.update(input);
+
+    return `${algorithm} hash for input:\n\n\`${hash.digest('hex')}\``;
+  },
+};
