d3da2ff5baaeabb99cdf6a16451cb45f3f4b4775
Marcel Menzel <mail@mcl.gg>
2019-05-11 22:25:34 +0200

    since that table isn't needed anymore, just delete the migration files or it

--- migrations/20190401111338-drop-member-last-message.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.dropTable('member_last_message'),
-  down: (queryInterface, Sequelize) => queryInterface.sequelize.query('select 1+1'), // nah
-};
