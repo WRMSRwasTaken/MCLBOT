46884ab5ebde393b4175ccfb0962d8e6a541b872
Marcel Menzel <mail@mcl.gg>
2019-05-12 01:29:24 +0200

    added missing await keyword

--- migrations/20190422185502-convert-name-logs-type-to-enum.js
+++ migrations/20190422185502-convert-name-logs-type-to-enum.js
@@ -1,7 +1,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(async (t) => {
     await queryInterface.renameColumn('name_logs', 'type', 'type_old', { transaction: t });
-    queryInterface.addColumn('name_logs', 'type', {
+    await queryInterface.addColumn('name_logs', 'type', {
       type: Sequelize.ENUM('USERNAME', 'DISCRIMINATOR', 'TAG', 'NICKNAME'),
     }, { transaction: t });
     await queryInterface.sequelize.query('update name_logs set type = (enum_range(null::enum_name_logs_type))[type_old]', { transaction: t });
