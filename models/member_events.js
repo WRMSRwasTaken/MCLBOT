848df474a71aaee48741b035abf87583e36e9d33
Marcel Menzel <mail@mcl.gg>
2019-05-12 18:55:59 +0200

    missing composite key declarations

--- models/member_events.js
+++ models/member_events.js
@@ -2,18 +2,22 @@ module.exports = (sequelize, DataTypes) => {
   const reminders = sequelize.define('member_events', {
     user_id: {
       allowNull: false,
+      primaryKey: true,
       type: DataTypes.BIGINT,
     },
     guild_id: {
       allowNull: false,
+      primaryKey: true,
       type: DataTypes.BIGINT,
     },
     type: {
       allowNull: false,
+      primaryKey: true,
       type: DataTypes.ENUM('JOIN', 'LEAVE'),
     },
     timestamp: {
       allowNull: false,
+      primaryKey: true,
       type: DataTypes.DATE,
     },
   }, {
