a4708d47828242d45c54fce8798d0d9ad6b077d4
Marcel Menzel <mail@mcl.gg>
2019-07-19 00:47:11 +0200

    use fapi for magik command

--- commands/image/magik.js
+++ commands/image/magik.js
@@ -1,6 +1,19 @@
+// module.exports = {
+//   alias: ['imagemagic', 'imagemagick', 'magic', 'magick', 'cas', 'liquid'],
+//   description: 'rescales an image with seam-carving multiple times leading to image distortion',
+//   arguments: [
+//     {
+//       label: 'url | user | emoji',
+//       type: 'image',
+//       optional: true,
+//     },
+//   ],
+//   fn: async (ctx, image) => ctx.main.imageHelper.processImage(ctx, image, null, '-liquid-rescale 50% -liquid-rescale 150%'),
+// };
+
 module.exports = {
   alias: ['imagemagic', 'imagemagick', 'magic', 'magick', 'cas', 'liquid'],
-  description: 'rescales an image with seam-carving multiple times leading to image distortion',
+  description: 'Rescales an image with seam-carving multiple times leading to image distortion',
   arguments: [
     {
       label: 'url | user | emoji',
@@ -8,5 +21,15 @@ module.exports = {
       optional: true,
     },
   ],
-  fn: async (ctx, image) => ctx.main.imageHelper.processImage(ctx, image, null, '-liquid-rescale 50% -liquid-rescale 150%'),
+  fn: async (ctx, image) => ctx.main.imageHelper.fAPI(ctx, 'evalmagik', {
+    images: [image],
+    args: {
+      text: [
+        '-liquid-rescale',
+        '50%',
+        '-liquid-rescale',
+        '150%',
+      ],
+    },
+  }),
 };
