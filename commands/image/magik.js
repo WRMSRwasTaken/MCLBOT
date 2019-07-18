4ca6e68bb03fd3532c98bc883d9575daba06dfb8
Marcel Menzel <mail@mcl.gg>
2019-07-19 00:50:18 +0200

    yes

--- commands/image/magik.js
+++ commands/image/magik.js
@@ -1,16 +1,3 @@
-// module.exports = {
-//   alias: ['imagemagic', 'imagemagick', 'magic', 'magick', 'cas', 'liquid'],
-//   description: 'rescales an image with seam-carving multiple times leading to image distortion',
-//   arguments: [
-//     {
-//       label: 'url | user | emoji',
-//       type: 'image',
-//       optional: true,
-//     },
-//   ],
-//   fn: async (ctx, image) => ctx.main.imageHelper.processImage(ctx, image, null, '-liquid-rescale 50% -liquid-rescale 150%'),
-// };
-
 module.exports = {
   alias: ['imagemagic', 'imagemagick', 'magic', 'magick', 'cas', 'liquid'],
   description: 'Rescales an image with seam-carving multiple times leading to image distortion',
