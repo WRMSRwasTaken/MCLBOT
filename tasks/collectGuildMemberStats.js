f573e415663c7e629ba63cf940d3a7d74f498674
Marcel Menzel <mail@mcl.gg>
2019-09-21 22:35:16 +0200

    Catch errors writing to the database to prevent guild stats collection working at all till next reboot

--- tasks/collectGuildMemberStats.js
+++ tasks/collectGuildMemberStats.js
@@ -9,13 +9,17 @@ module.exports = {
     for (const guild of main.api.guilds.values()) {
       winston.debug('Collecting stats for guild: %s...', guild.name);
 
-      main.prometheusMetrics.sqlCommands.labels('INSERT').inc();
-      await main.db.guild_member_counts.create({
-        guild_id: guild.id,
-        timestamp: Date.now(),
-        members_online: guild.members.filter(c => c.presence.status !== 'offline').size,
-        members_total: guild.memberCount,
-      });
+      main.prometheusMetrics.sqlCommands.labels('INSERT').inc(); // TODO: I propably should move the metrics after actually calling the IO functions, to see any errors impacting the stats
+      try {
+        await main.db.guild_member_counts.create({
+          guild_id: guild.id,
+          timestamp: Date.now(),
+          members_online: guild.members.filter(c => c.presence.status !== 'offline').size,
+          members_total: guild.memberCount,
+        });
+      } catch (ex) {
+        winston.error('There was an error writing guild stats for guild %s: %s', guild.id, ex.message);
+      }
     }
 
     winston.debug('Collecting guild stats finished after %s', prettyMs(Date.now() - startTime));
