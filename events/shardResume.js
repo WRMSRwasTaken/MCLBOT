036fb71fb1265782c56081157577899399ab5644
Marcel Menzel <mail@mcl.gg>
2019-12-13 18:40:28 +0100

    fix erroring on resuming the ws connection

--- events/shardResume.js
+++ events/shardResume.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const prettyMs = require('pretty-ms');
 
 module.exports = {
-  fn: async (main, replayed) => {
+  fn: async (main, id, replayed) => {
     if (!main.firstReady) {
       winston.error('Resumed event has been fired although there was no ready event before! Exiting...'); // yes this already happened when MCLBOT tried to log in while Discord had problems
 
@@ -12,11 +12,11 @@ module.exports = {
     main.ready = true;
     main.onlineTime = Date.now();
 
-    winston.info(`Websocket received RESUMED event after ${prettyMs(Date.now() - this.preConnectTime)}. Replayed ${replayed} event(s).`);
+    winston.info(`Websocket received RESUMED event after ${prettyMs(Date.now() - this.main.preConnectTime)}. Replayed ${replayed} event(s).`);
 
     return main.channelLogHelper.sendLogMessage('resumed', {
       replayed,
-      reconnectDuration: Date.now() - this.preConnectTime,
+      reconnectDuration: Date.now() - this.main.preConnectTime,
     });
   },
 };
